a613031b239dcfb21a00c0aa70728e4ffb88c253
Jernej Kos <k@jst.sm>
2013-10-06 01:27:28 +0200

    Improved readability.

--- datastream/backends/mongodb.py
+++ datastream/backends/mongodb.py
@@ -311,8 +311,7 @@ class DerivationOperators(object):
     def get(cls, operator):
         if not hasattr(cls, '_values'):
             cls._values = {
-                getattr(cls, name).name: getattr(cls, name)
-                for name in cls.__dict__ if
+                getattr(cls, name).name: getattr(cls, name) for name in cls.__dict__ if
                     name != 'get' and inspect.isclass(getattr(cls, name)) and getattr(cls, name) is not cls._Base and issubclass(getattr(cls, name), cls._Base)
             }
 
